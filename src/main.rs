c92765662f44cb9449c7c841e6f37878bff84b40
Uri Baghin <Coneko@users.noreply.github.com>
2018-02-10 10:54:07 +1100

    Push the base branch for the PR.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(nll)]
 #![recursion_limit = "1024"]
 extern crate clap;
 extern crate env_logger;
@@ -12,26 +13,17 @@ extern crate regex;
 extern crate tokio_core;
 
 mod errors {
-    error_chain!{
-        links {
-            Hubcaps(::hubcaps::errors::Error, ::hubcaps::errors::ErrorKind);
-        }
-        foreign_links {
-            Fmt(::std::fmt::Error);
-            Io(::std::io::Error);
-        }
-    }
+    error_chain!{}
 }
 
 use errors::*;
-use futures::Stream;
 
 quick_main!(run);
 
 fn run() -> Result<i32> {
     env_logger::init();
 
-    let prog = std::env::current_exe()
+    let prog: String = std::env::current_exe()
         .expect("Couldn't get program name.")
         .file_name()
         .expect("No file found.")
@@ -52,41 +44,65 @@ fn run() -> Result<i32> {
 }
 
 fn run_up() -> Result<i32> {
-    let repo = git2::Repository::discover(".")
+    let pr_branch_prefix: String = format!(
+        "{}-stack-",
+        std::env::var("USER").chain_err(|| {
+            "No USER environment variable found, cannot get current user's username."
+        })?
+    );
+    let pr_branch_postfix = "-pr";
+    let pr_base_branch_postfix = "-base";
+
+    let repo: git2::Repository = git2::Repository::discover(".")
         .chain_err(|| "Not a git repository (or any of the parent directories).")?;
-    let origin = repo.find_remote("origin")
+    let mut origin: git2::Remote = repo.find_remote("origin")
         .chain_err(|| "Could not find remote origin.")?;
-    let re = regex::Regex::new(r"^git@github\.com:(?P<owner>[^/]+)/(?P<repo>.+)\.git$")
-        .chain_err(|| "Could not construct regex.")?;
-    debug!("Remote origin url: '{:?}'", origin.url());
+    let re: regex::Regex = regex::Regex::new(
+        r"^git@github\.com:(?P<owner>[^/]+)/(?P<repo>.+)\.git$",
+    ).chain_err(|| "Could not construct regex.")?;
     let captures = re.captures(origin.url().ok_or("Could not read remote origin url.")?)
         .ok_or("Could not extract Github repo from origin url.")?;
     let github_owner = captures
         .name("owner")
         .ok_or("Could not find github owner in origin url.")?
         .as_str();
-    debug!("Github owner: '{}'", github_owner);
     let github_repo = captures
         .name("repo")
         .ok_or("Could not find github repo in origin url.")?
         .as_str();
-    debug!("Github repo: '{}'", github_repo);
-    let token =
+    let token: String =
         std::env::var("GITHUB_TOKEN").chain_err(|| "No GITHUB_TOKEN environment variable found.")?;
 
-    let mut core = tokio_core::reactor::Core::new().chain_err(|| "Could not create new core.")?;
+    let mut core: tokio_core::reactor::Core =
+        tokio_core::reactor::Core::new().chain_err(|| "Could not create new core.")?;
     let github = hubcaps::Github::new(
         concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION")),
         Some(hubcaps::Credentials::Token(token)),
         &core.handle(),
     );
-    let repo = github.repo(github_owner, github_repo);
-    let pulls = repo.pulls();
-    core.run(
-        pulls
-            .iter(&Default::default())
-            .for_each(|pull| Ok(println!("{:#?}", pull))),
-    )?;
+    let github_repo = github.repo(github_owner, github_repo);
+
+    let head_commit: git2::Commit = repo.head()
+        .chain_err(|| "Could not get HEAD reference.")?
+        .peel_to_commit()
+        .chain_err(|| "Could not get commit referenced by HEAD.")?;
+    let mut parents = head_commit.parents();
+    let parent = parents.next().ok_or("HEAD commit has no parents.")?;
+    if parents.next().is_some() {
+        bail!("HEAD commit has more than one parent.");
+    }
+    let base_branch: &str = &format!(
+        "{}{}{}",
+        pr_branch_prefix,
+        head_commit.id(),
+        pr_base_branch_postfix
+    );
+    let pr_base_branch: git2::Branch = repo.branch(base_branch, &parent, false)
+        .chain_err(|| format!("Could not create branch at parent '{}'", parent.id()))?;
+
+    origin
+        .push(&[base_branch], Default::default())
+        .chain_err(|| "Couldn't push PR base branch.")?;
 
     Ok(0)
 }
